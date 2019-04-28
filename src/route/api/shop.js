29a3f6ccd98acd9f32065534bc22ffc39710ff31
rjwu95 <yugun000@gmail.com>
2019-04-28 23:01:19 +0900

    Add converLocation route

--- src/route/api/shop.js
+++ src/route/api/shop.js
@@ -1,8 +1,10 @@
 const route = require('express').Router();
 const { getLogger } = require('../../../config');
+const axios = require('axios');
 const Shop = require('../../models/shop');
 
 const logger = getLogger('Shops');
+const { mapApiKey } = require('../../../config/mongoose.json');
 
 const LAT_DISTANCE = 0.0018; // 약 200m
 const LNG_DISTANCE = 0.00227; // 약 200m
@@ -37,6 +39,21 @@ route.get('/currentLocation', async (req, res) => {
     return res.status(200).send(shopList);
   } catch (err) {
     return logger.log(err);
+  }
+});
+
+route.get('/convertLocation', async (req, res) => {
+  const { latitude, longitude } = req.headers;
+  const result = await axios.get(
+    `https://maps.googleapis.com/maps/api/geocode/json?latlng=${latitude},${longitude}&key=${mapApiKey}&language=ko`,
+  );
+  const defineLocation = result.data.results[0].formatted_address;
+  if (defineLocation.match(/.*동/)) {
+    return res.status(200).send(defineLocation.match(/.*동/)[0].slice(5));
+  } else if (defineLocation.match(/.*읍/)) {
+    return res.status(200).send(defineLocation.match(/.*읍/)[0].slice(5));
+  } else {
+    return res.status(200).send(defineLocation.match(/.*면/)[0].slice(5));
   }
 });
 
